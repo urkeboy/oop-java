3aa6b7c57b9495f2a185e16eec3653e3422d3483
uros_bojanic <bu190077d@student.etf.bg.ac.rs>
2022-03-10 19:20:43 +0100

    Problem 2 - Solution

---
+++ Problem 2 - Roulette game/src/test/Test.java
@@ -0,0 +1,21 @@
+package test;
+
+import solution.Generator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Generator g1 = new Generator(0, 10);
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+		System.out.println(g1.slucajanBroj());
+	}
+	
+}
